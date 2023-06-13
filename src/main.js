ad8072f88457a332c1e7105ad81b48b2090f0239
yangjp <1257282045@qq.com>
2023-06-13 11:14:20 +0800

    yjp提交了部分界面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/css/global.css'
+// element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ELIcons from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+    .use(ElementPlus, {
+        locale: zhCn
+    })
+for (let iconName in ELIcons) {
+    app.component(iconName, ELIcons[iconName])
+}
+app.use(router)
+app.use(store)
+app.mount('#app')
+
